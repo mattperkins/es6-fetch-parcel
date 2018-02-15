0174f06d51c4b3152e0147f44b2b135cc3385706
mattperkins <github@mattperkins.com>
2018-02-15 18:26:07 +0000

    correct errors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ searchForm.addEventListener('submit', e =>{
     const searchLimit = document.getElementById('limit').value
     
     // check input
-    if(searchTerm === '') {
+    if(searchTerm === ' ') {
         // Show message
         showMessage('Please add a search term', 'alert-danger')
     }
@@ -24,28 +24,28 @@ searchForm.addEventListener('submit', e =>{
     // Search API
     reddit.search(searchTerm, searchLimit, sortBy)
         .then(results => {
-            console.log(results)
-            let output = '<div class="card-columns">'
-            // Loop through posts
-            results.forEach(post => {
-                // Check for image
-                const image = post.preview ? post.preview.images[0].source.url : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg'
+        console.log(results)
+        let output = '<div class="card-columns">'
+        // Loop through posts
+        results.forEach(post => {
+            // Check for image
+            const image = post.preview ? post.preview.images[0].source.url : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg'
 
-                output += `
-                    <div class="card">
-                        <img class="card-img-top" src="${image}" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">${post.title}</h5>
-                            <p class="card-text">${truncateText(post.selftext, 100)}</p>
-                            <a href="${post.url}" target="_blank" class="btn btn-primary">Read More</a>
-                            <hr>
-                            <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
-                            <span class="badge badge-dark">Score: ${post.score}</span>
-                        </div>
-                    </div>   
-                `
-            })
-            output += '<div>'
+            output += `
+                <div class="card">
+                    <img class="card-img-top" src="${image}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${post.title}</h5>
+                        <p class="card-text">${truncateText(post.selftext, 100)}</p>
+                        <a href="${post.url}" target="_blank" class="btn btn-primary">Read More</a>
+                        <hr>
+                        <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
+                        <span class="badge badge-dark">Score: ${post.score}</span>
+                    </div>
+                </div>   
+            `
+        })
+            output += '</div>'
             document.getElementById('results').innerHTML = output
     })
 
